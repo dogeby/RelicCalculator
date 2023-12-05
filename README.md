272f70dd0b8d24d40b21f5b9f617834fe99849f0
dogeby <dogeby@gmail.com>
2023-12-05 17:41:01 +0900

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# relic-calculator
+## 앱 소개
+## 앱 UI 디자인
+https://www.figma.com/file/FJiek3VfaXXuLNHK9pO6ki/RelicCalculator?type=design&node-id=0%3A1&mode=design&t=PZJfAtgsK2TZYiY6-1
+## 기술 스킬
+## 주요 기능
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+}
