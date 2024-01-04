384b118f1dd1a17824dbf6a31d04187efd7becb1
dogeby <dogeby@gmail.com>
2024-01-04 23:40:21 +0900

    Add serialization json and junit lib

--- rating/build.gradle.kts
+++ rating/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("kotlin")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
     implementation(project(":core:model"))
+    implementation(libs.kotlinx.serialization.json)
+
+    testImplementation(libs.junit)
 }
