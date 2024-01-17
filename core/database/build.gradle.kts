8eae2375e5c05c6f75bbe0f680783547541a0cf8
dogeby <dogeby@gmail.com>
2024-01-18 07:02:12 +0900

    Add test, kotlinx.serialization libraries and model module to database module

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.org.jetbrains.kotlin.kapt)
 }
@@ -35,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:model"))
     implementation(libs.core.ktx)
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
@@ -42,7 +44,9 @@ dependencies {
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     kapt(libs.room.compiler)
+    implementation(libs.kotlinx.serialization.json)
 
-    testImplementation(libs.junit)
-    testImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
 }
