2529838c8c9fb2f6ec1d7ce0c5ee8e91cc0c3179
dogeby <dogeby@gmail.com>
2024-01-28 21:24:10 +0900

    Remove id property from CharacterPreset.kt

--- core/model/src/main/java/com/dogeby/reliccalculator/core/model/data/preset/CharacterPreset.kt
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/data/preset/CharacterPreset.kt
@@ -1,7 +1,6 @@
 package com.dogeby.reliccalculator.core.model.data.preset
 
 data class CharacterPreset(
-    val id: String,
     val characterId: String,
     val relicSetIds: List<String>,
     val relicStatWeights: List<RelicStatWeight>,
