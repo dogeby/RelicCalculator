d3b85ea5e4a47511f0121b4ef0a988a264017f43
dogeby <dogeby@gmail.com>
2024-02-09 12:34:40 +0900

    Add Singleton annotation to bindsInternalStorageManager

---
+++ core/data/src/main/java/com/dogeby/core/data/di/DataModule.kt
@@ -0,0 +1,20 @@
+package com.dogeby.core.data.di
+
+import com.dogeby.core.data.repository.PresetRepository
+import com.dogeby.core.data.repository.PresetRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindsPresetRepository(
+        presetRepositoryImpl: PresetRepositoryImpl,
+    ): PresetRepository
+}
