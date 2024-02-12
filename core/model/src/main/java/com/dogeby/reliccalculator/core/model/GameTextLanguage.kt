461654316b6d952de80a5ad53691eb526c4c8d48
dogeby <dogeby@gmail.com>
2024-02-12 19:42:03 +0900

    Add GameTextLanguage and AppPreferencesDataSource

---
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/GameTextLanguage.kt
@@ -0,0 +1,17 @@
+package com.dogeby.reliccalculator.core.model
+
+enum class GameTextLanguage(val code: String) {
+    CHT("cht"),
+    CN("cn"),
+    DE("de"),
+    EN("en"),
+    ES("es"),
+    FR("fr"),
+    ID("id"),
+    JP("jp"),
+    KR("kr"),
+    PT("pt"),
+    RU("ru"),
+    TH("th"),
+    VI("vi"),
+}
---
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/preferences/AppPreferencesData.kt
@@ -0,0 +1,7 @@
+package com.dogeby.reliccalculator.core.model.preferences
+
+import com.dogeby.reliccalculator.core.model.GameTextLanguage
+
+data class AppPreferencesData(
+    val gameTextLanguage: GameTextLanguage,
+)
