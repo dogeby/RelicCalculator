b648f180d9af85715ccf9528d6678c79e8080a82
dogeby <dogeby@gmail.com>
2024-02-15 13:22:44 +0900

    Add icon property to RelicPiece (#63)

--- core/model/src/main/java/com/dogeby/reliccalculator/core/model/hoyo/index/RelicPiece.kt
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/hoyo/index/RelicPiece.kt
@@ -1,10 +1,10 @@
 package com.dogeby.reliccalculator.core.model.hoyo.index
 
-enum class RelicPiece {
-    HEAD,
-    HAND,
-    BODY,
-    FOOT,
-    NECK,
-    OBJECT,
+enum class RelicPiece(val icon: String) {
+    HEAD("icon/piece/head.webp"),
+    HAND("icon/piece/hand.webp"),
+    BODY("icon/piece/body.webp"),
+    FOOT("icon/piece/foot.webp"),
+    NECK("icon/piece/neck.webp"),
+    OBJECT("icon/piece/object.webp"),
 }
