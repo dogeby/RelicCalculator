ba454b54827c6d3ba25d927c9433b1970b5385a3
dogeby <dogeby@gmail.com>
2024-02-17 18:05:33 +0900

    Add PresetWithDetails to model module (#71)
    * Add AffixWeightWithInfo
    * Add AttrComparisonWithInfo

---
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/preset/AttrComparisonWithInfo.kt
@@ -0,0 +1,8 @@
+package com.dogeby.reliccalculator.core.model.preset
+
+import com.dogeby.reliccalculator.core.model.hoyo.index.PropertyInfo
+
+data class AttrComparisonWithInfo(
+    val attrComparison: AttrComparison,
+    val propertyInfo: PropertyInfo,
+)
