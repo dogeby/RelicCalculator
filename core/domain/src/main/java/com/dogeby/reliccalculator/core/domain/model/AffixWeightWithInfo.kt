09b8b99761b4ffc0c619f48bf07b937d37ba2a02
dogeby <dogeby@gmail.com>
2024-02-18 19:14:46 +0900

    Modify AffixWeightWithInfo property from AffixInfo to PropertyInfo

--- core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/model/AffixWeightWithInfo.kt
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/model/AffixWeightWithInfo.kt
@@ -1,9 +1,9 @@
 package com.dogeby.reliccalculator.core.domain.model
 
-import com.dogeby.reliccalculator.core.model.hoyo.index.AffixInfo
+import com.dogeby.reliccalculator.core.model.hoyo.index.PropertyInfo
 import com.dogeby.reliccalculator.core.model.preset.AffixWeight
 
 data class AffixWeightWithInfo(
     val affixWeight: AffixWeight,
-    val affixInfo: AffixInfo,
+    val propertyInfo: PropertyInfo,
 )
