a4b65c1bf14788ba1749ec20cd3b77d183466aa2
dogeby <dogeby@gmail.com>
2024-02-18 19:15:05 +0900

    Add Preset.toPresetWithDetails function

--- core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/model/PresetWithDetails.kt
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/model/PresetWithDetails.kt
@@ -5,6 +5,7 @@ import com.dogeby.reliccalculator.core.model.hoyo.index.ElementInfo
 import com.dogeby.reliccalculator.core.model.hoyo.index.PathInfo
 import com.dogeby.reliccalculator.core.model.hoyo.index.RelicPiece
 import com.dogeby.reliccalculator.core.model.hoyo.index.RelicSetInfo
+import com.dogeby.reliccalculator.core.model.preset.Preset
 
 data class PresetWithDetails(
     val characterId: String,
@@ -16,4 +17,24 @@ data class PresetWithDetails(
     val subAffixWeightsWithInfo: List<AffixWeightWithInfo>,
     val isAutoUpdate: Boolean = true,
     val attrComparisons: List<AttrComparisonWithInfo>,
+)
+
+fun Preset.toPresetWithDetails(
+    characterInfo: CharacterInfo,
+    pathInfo: PathInfo,
+    elementInfo: ElementInfo,
+    relicSets: List<RelicSetInfo>,
+    pieceMainAffixWeightsWithInfo: Map<RelicPiece, List<AffixWeightWithInfo>>,
+    subAffixWeightsWithInfo: List<AffixWeightWithInfo>,
+    attrComparisons: List<AttrComparisonWithInfo>,
+) = PresetWithDetails(
+    characterId = characterId,
+    characterInfo = characterInfo,
+    pathInfo = pathInfo,
+    elementInfo = elementInfo,
+    relicSets = relicSets,
+    pieceMainAffixWeightsWithInfo = pieceMainAffixWeightsWithInfo,
+    subAffixWeightsWithInfo = subAffixWeightsWithInfo,
+    isAutoUpdate = isAutoUpdate,
+    attrComparisons = attrComparisons,
 )
