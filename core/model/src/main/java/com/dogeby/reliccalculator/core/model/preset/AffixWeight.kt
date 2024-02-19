1131a5909c0dfd1eb8e5df48b8b78a637c29dbcd
dogeby <dogeby@gmail.com>
2024-02-19 22:12:04 +0900

    Add sampleAffixWeight, sampleAttrComparison (#83)

--- core/model/src/main/java/com/dogeby/reliccalculator/core/model/preset/AffixWeight.kt
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/preset/AffixWeight.kt
@@ -2,10 +2,25 @@ package com.dogeby.reliccalculator.core.model.preset
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.jetbrains.annotations.TestOnly
 
 @Serializable
 data class AffixWeight(
     @SerialName("affix_id") val affixId: String,
     val type: String,
     val weight: Float,
+)
+
+@TestOnly
+val sampleMainAffixWeight = AffixWeight(
+    affixId = "1",
+    type = "HPDelta",
+    weight = 1.0f,
+)
+
+@TestOnly
+val sampleSubAffixWeight = AffixWeight(
+    affixId = "2",
+    type = "AttackDelta",
+    weight = 1.0f,
 )
