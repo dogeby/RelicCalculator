a76853d018624a4417904ded20b0da895b7c685e
dogeby <dogeby@gmail.com>
2024-02-19 22:02:03 +0900

    Add samplePropertyInfo, sampleRelicSetInfo (#83)

--- core/model/src/main/java/com/dogeby/reliccalculator/core/model/hoyo/index/PropertyInfo.kt
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/hoyo/index/PropertyInfo.kt
@@ -1,6 +1,7 @@
 package com.dogeby.reliccalculator.core.model.hoyo.index
 
 import kotlinx.serialization.Serializable
+import org.jetbrains.annotations.TestOnly
 
 @Serializable
 data class PropertyInfo(
@@ -12,4 +13,16 @@ data class PropertyInfo(
     val percent: Boolean,
     val order: Int,
     val icon: String,
+)
+
+@TestOnly
+val samplePropertyInfo = PropertyInfo(
+    type = "SpeedDelta",
+    name = "속도",
+    field = "spd",
+    affix = true,
+    ratio = false,
+    percent = false,
+    order = 100,
+    icon = "icon/property/IconSpeed.png",
 )
--- core/model/src/main/java/com/dogeby/reliccalculator/core/model/hoyo/index/RelicSetInfo.kt
+++ core/model/src/main/java/com/dogeby/reliccalculator/core/model/hoyo/index/RelicSetInfo.kt
@@ -1,6 +1,7 @@
 package com.dogeby.reliccalculator.core.model.hoyo.index
 
 import kotlinx.serialization.Serializable
+import org.jetbrains.annotations.TestOnly
 
 @Serializable
 data class RelicSetInfo(
@@ -8,4 +9,15 @@ data class RelicSetInfo(
     val name: String,
     val desc: List<String>,
     val icon: String,
+)
+
+@TestOnly
+val sampleRelicSetInfo = RelicSetInfo(
+    id = "104",
+    name = "혹한 밀림의 사냥꾼",
+    desc = listOf(
+        "얼음 속성 피해 10% 증가",
+        "장착한 캐릭터는 필살기 발동 시 치명타 피해가 25% 증가한다. 지속 시간: 2턴",
+    ),
+    icon = "icon/relic/104.png",
 )
