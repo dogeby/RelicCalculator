2f4de6ac139755c4e354f2dda82ddcd6c7b497e9
dogeby <dogeby@gmail.com>
2024-02-21 19:20:22 +0900

    Add UpdatePresetAutoUpdateUseCase (#86)

---
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/preset/UpdatePresetAutoUpdateUseCase.kt
@@ -0,0 +1,16 @@
+package com.dogeby.reliccalculator.core.domain.preset
+
+import com.dogeby.core.data.repository.PresetRepository
+import javax.inject.Inject
+
+class UpdatePresetAutoUpdateUseCase @Inject constructor(
+    private val presetRepository: PresetRepository,
+) {
+
+    suspend operator fun invoke(
+        ids: Set<String>,
+        isAutoUpdate: Boolean,
+    ): Result<Int> {
+        return presetRepository.updatePresetsAutoUpdate(ids, isAutoUpdate)
+    }
+}
