0c59002dd7ad050067b1792660524d5ddaf15cbc
dogeby <dogeby@gmail.com>
2024-02-22 23:58:16 +0900

    Move AffixImage to GameImage.kt (#99)
    * Removed AffixImage.kt
    * Modified String.modifyExtension visibility from public to private
    * Modified AffixImage logic from AsyncImage to GameImage

--- core/ui/src/main/java/com/dogeby/core/ui/component/image/GameImage.kt
+++ core/ui/src/main/java/com/dogeby/core/ui/component/image/GameImage.kt
@@ -22,7 +22,16 @@ import com.dogeby.core.ui.R
 import com.dogeby.core.ui.theme.RelicCalculatorTheme
 
 private const val GAME_IMAGE_PATH = "file:///android_asset/game/"
+private const val AFFIX_IMAGE_PATH = "icon/affix/"
+
 private const val GAME_IMAGE_EXTENSION = "webp"
+
+private fun String.modifyExtension(newExtension: String): String {
+    if ('.' in newExtension) {
+        throw IllegalArgumentException("Invalid extension: $newExtension")
+    }
+    return replaceAfterLast('.', newExtension, "$this.$newExtension")
+}
 
 @Composable
 fun GameImage(
@@ -63,11 +72,41 @@ fun GameImage(
     )
 }
 
-fun String.modifyExtension(newExtension: String): String {
-    if ('.' in newExtension) {
-        throw IllegalArgumentException("Invalid extension: $newExtension")
-    }
-    return replaceAfterLast('.', newExtension, "$this.$newExtension")
+@Composable
+fun AffixImage(
+    type: String,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    placeholder: Painter? = null,
+    error: Painter? = rememberVectorPainter(image = Icons.Default.BrokenImage),
+    fallback: Painter? = error,
+    onLoading: ((AsyncImagePainter.State.Loading) -> Unit)? = null,
+    onSuccess: ((AsyncImagePainter.State.Success) -> Unit)? = null,
+    onError: ((AsyncImagePainter.State.Error) -> Unit)? = null,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Fit,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+    filterQuality: FilterQuality = DrawScope.DefaultFilterQuality,
+    crossFadeDurationMillis: Int = integerResource(id = R.integer.DEFAULT_CROSS_FADE_DURATION),
+) {
+    GameImage(
+        src = AFFIX_IMAGE_PATH + type,
+        contentDescription = contentDescription,
+        modifier = modifier,
+        placeholder = placeholder,
+        error = error,
+        fallback = fallback,
+        onLoading = onLoading,
+        onSuccess = onSuccess,
+        onError = onError,
+        alignment = alignment,
+        contentScale = contentScale,
+        alpha = alpha,
+        colorFilter = colorFilter,
+        filterQuality = filterQuality,
+        crossFadeDurationMillis = crossFadeDurationMillis,
+    )
 }
 
 @Preview(showBackground = true)
@@ -76,6 +115,17 @@ private fun GameImagePreview() {
     RelicCalculatorTheme {
         GameImage(
             src = "icon/property/IconSpeed.png",
+            placeholder = rememberVectorPainter(image = Icons.Default.BrokenImage),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AffixImagePreview() {
+    RelicCalculatorTheme {
+        AffixImage(
+            type = "AttackAddedRatio",
             placeholder = rememberVectorPainter(image = Icons.Default.BrokenImage),
         )
     }
