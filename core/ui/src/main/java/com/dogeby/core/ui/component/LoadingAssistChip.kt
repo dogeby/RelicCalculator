9e8fefc7913a995c1f03ab86d8087c0ba58b98c5
dogeby <dogeby@gmail.com>
2024-02-26 02:04:59 +0900

    Add LoadingAssistChip to ui module (#107)

---
+++ core/ui/src/main/java/com/dogeby/core/ui/component/LoadingAssistChip.kt
@@ -0,0 +1,61 @@
+package com.dogeby.core.ui.component
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Update
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dogeby.core.ui.theme.RelicCalculatorTheme
+
+@Composable
+fun LoadingAssistChip(
+    isLoading: () -> Boolean,
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    AssistChip(
+        onClick = onClick,
+        label = { Text(text = text) },
+        modifier = modifier,
+        leadingIcon = {
+            AnimatedContent(
+                targetState = isLoading(),
+                label = "LoadingAssistChipLeadingIconAnimation",
+            ) {
+                if (it) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(AssistChipDefaults.IconSize),
+                        strokeWidth = 2.dp,
+                    )
+                    return@AnimatedContent
+                }
+                if (leadingIcon != null) {
+                    leadingIcon()
+                }
+            }
+        },
+    )
+}
+
+@Preview(apiLevel = 33)
+@Composable
+private fun PreviewLoadingAssistChip() {
+    RelicCalculatorTheme {
+        LoadingAssistChip(
+            isLoading = { false },
+            text = "test",
+            onClick = {},
+            leadingIcon = { Icon(imageVector = Icons.Default.Update, contentDescription = null) },
+        )
+    }
+}
