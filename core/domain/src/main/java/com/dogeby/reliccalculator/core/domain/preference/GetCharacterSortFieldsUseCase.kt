73b9e1f72a940ae28acc5efd24227615c26c2382
dogeby <dogeby@gmail.com>
2024-02-27 15:51:53 +0900

    Remove GetCharacterSortFieldsUseCase from domain module (#110)

--- core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/preference/GetCharacterSortFieldsUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.dogeby.reliccalculator.core.domain.preference
-
-import com.dogeby.reliccalculator.core.model.preferences.CharacterSortField
-import javax.inject.Inject
-
-class GetCharacterSortFieldsUseCase @Inject constructor() {
-
-    operator fun invoke() = CharacterSortField.entries.toSet()
-}
