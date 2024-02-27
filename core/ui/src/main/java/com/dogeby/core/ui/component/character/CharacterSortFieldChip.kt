64de39481d11542338cd11783c8cc465ec446d99
dogeby <dogeby@gmail.com>
2024-02-27 16:09:01 +0900

    Modify CharacterSortDropdownMenuItem verticalAlignment to CenterVertically (#107)

--- core/ui/src/main/java/com/dogeby/core/ui/component/character/CharacterSortFieldChip.kt
+++ core/ui/src/main/java/com/dogeby/core/ui/component/character/CharacterSortFieldChip.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,7 +58,7 @@ fun CharacterSortDropdownMenuItem(
 ) {
     DropdownMenuItem(
         text = {
-            Row {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(
                     text = stringResource(
                         id = when (field) {
