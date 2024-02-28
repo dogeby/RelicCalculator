d074a457b506ba711fc42a32a4677859c041f216
dogeby <dogeby@gmail.com>
2024-02-29 07:49:27 +0900

    Add PresetsNavigation (#11)

---
+++ feature/presets/src/main/java/com/dogeby/reliccalculator/feature/presets/navigation/PresetsNavigation.kt
@@ -0,0 +1,21 @@
+package com.dogeby.reliccalculator.feature.presets.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.dogeby.reliccalculator.feature.presets.PresetsRoute
+
+const val PRESETS_ROUTE = "presets_route"
+
+fun NavController.navigateToPresets(navOptions: NavOptions? = null) {
+    navigate(PRESETS_ROUTE, navOptions)
+}
+
+fun NavGraphBuilder.presetsScreen(navigateToPresetEdit: (String) -> Unit) {
+    composable(route = PRESETS_ROUTE) {
+        PresetsRoute(
+            navigateToPresetEdit = navigateToPresetEdit,
+        )
+    }
+}
