e3795454ba286cfabdc14e255f17f1a181ff663b
dogeby <dogeby@gmail.com>
2024-03-07 10:49:34 +0900

    Add FilterChipWithRichTooltip to FilterChipWithTooltip (#132)

--- core/ui/src/main/java/com/dogeby/reliccalculator/core/ui/component/FilterChipWithTooltip.kt
+++ core/ui/src/main/java/com/dogeby/reliccalculator/core/ui/component/FilterChipWithTooltip.kt
@@ -8,16 +8,20 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.RichTooltip
 import androidx.compose.material3.SelectableChipColors
 import androidx.compose.material3.SelectableChipElevation
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.TooltipState
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
@@ -25,6 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dogeby.reliccalculator.core.ui.component.image.GameImage
 import com.dogeby.reliccalculator.core.ui.theme.RelicCalculatorTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -70,6 +75,54 @@ fun FilterChipWithTooltip(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilterChipWithRichTooltip(
+    selected: () -> Boolean,
+    onClick: () -> Unit,
+    label: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    shape: Shape = FilterChipDefaults.shape,
+    colors: SelectableChipColors = FilterChipDefaults.filterChipColors(),
+    elevation: SelectableChipElevation? = FilterChipDefaults.filterChipElevation(),
+    border: BorderStroke? = FilterChipDefaults.filterChipBorder(enabled, selected()),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    tooltipState: TooltipState = rememberTooltipState(isPersistent = true),
+    tooltipTitle: (@Composable () -> Unit)? = null,
+    tooltipAction: (@Composable () -> Unit)? = null,
+    tooltipText: @Composable () -> Unit,
+) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+        tooltip = {
+            RichTooltip(
+                title = tooltipTitle,
+                action = tooltipAction,
+                text = tooltipText,
+            )
+        },
+        state = tooltipState,
+        modifier = modifier,
+    ) {
+        FilterChip(
+            selected = selected(),
+            onClick = onClick,
+            label = label,
+            enabled = enabled,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            shape = shape,
+            colors = colors,
+            elevation = elevation,
+            border = border,
+            interactionSource = interactionSource,
+        )
+    }
+}
+
 @Preview(apiLevel = 33)
 @Composable
 fun PreviewFilterChipWithTooltip() {
@@ -83,10 +136,49 @@ fun PreviewFilterChipWithTooltip() {
             label = {
                 GameImage(
                     src = "icon/relic/103.png",
-                    modifier = Modifier.padding(vertical = 4.dp).size(32.dp),
+                    modifier = Modifier
+                        .padding(vertical = 4.dp)
+                        .size(32.dp),
                 )
             },
             tooltipContent = { Text(text = "name") },
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(apiLevel = 33)
+@Composable
+fun PreviewFilterChipWithRichTooltip() {
+    var selected by remember {
+        mutableStateOf(false)
+    }
+    val tooltipState = rememberTooltipState(isPersistent = true)
+    val scope = rememberCoroutineScope()
+
+    RelicCalculatorTheme {
+        FilterChipWithRichTooltip(
+            selected = { selected },
+            onClick = { selected = !selected },
+            label = {
+                GameImage(
+                    src = "icon/relic/103.png",
+                    modifier = Modifier
+                        .padding(vertical = 4.dp)
+                        .size(32.dp),
+                )
+            },
+            tooltipState = tooltipState,
+            tooltipTitle = {
+                Text(text = "title")
+            },
+            tooltipAction = {
+                TextButton(
+                    onClick = { scope.launch { tooltipState.dismiss() } },
+                ) { Text("action") }
+            },
+        ) {
+            Text(text = "text")
+        }
     }
 }
