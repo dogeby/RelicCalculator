ccc5e8bdce1a96d1e3e1d874ecc3df4cc7b49eb5
dogeby <dogeby@gmail.com>
2024-03-11 15:22:55 +0900

    Add GetSubAffixWeightWithInfoUseCase to domain module (#141)

---
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/preset/GetSubAffixWeightWithInfoUseCase.kt
@@ -0,0 +1,38 @@
+package com.dogeby.reliccalculator.core.domain.preset
+
+import com.dogeby.reliccalculator.core.data.repository.GameRepository
+import com.dogeby.reliccalculator.core.domain.model.AffixWeightWithInfo
+import com.dogeby.reliccalculator.core.model.mihomo.index.AffixData
+import com.dogeby.reliccalculator.core.model.preset.AffixWeight
+import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+
+class GetSubAffixWeightWithInfoUseCase @Inject constructor(
+    private val gameRepository: GameRepository,
+) {
+
+    operator fun invoke(): Flow<List<AffixWeightWithInfo>> {
+        return gameRepository
+            .relicAffixInfoMap
+            .combine(gameRepository.propertyInfoMap) { relicAffixInfoMap, propertyInfoMap ->
+                relicAffixInfoMap.getOrDefault("5", AffixData("5", emptyMap()))
+                    .affixes
+                    .values
+                    .mapNotNull { affixInfo ->
+                        val affixWeight = AffixWeight(
+                            affixId = affixInfo.affixId,
+                            type = affixInfo.property,
+                            weight = 1.0f,
+                        )
+                        val propertyInfo = propertyInfoMap.getOrElse(affixInfo.property) {
+                            return@mapNotNull null
+                        }
+                        AffixWeightWithInfo(
+                            affixWeight = affixWeight,
+                            propertyInfo = propertyInfo,
+                        )
+                    }
+            }
+    }
+}
