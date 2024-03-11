30b4d063e0a76f37243e6956f73e4e071fc6bfef
dogeby <dogeby@gmail.com>
2024-03-11 10:25:57 +0900

    Add UpdatePresetUseCase to domain module (#142)

---
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/preset/UpdatePresetUseCase.kt
@@ -0,0 +1,36 @@
+package com.dogeby.reliccalculator.core.domain.preset
+
+import com.dogeby.reliccalculator.core.data.repository.PresetRepository
+import com.dogeby.reliccalculator.core.domain.model.AffixWeightWithInfo
+import com.dogeby.reliccalculator.core.domain.model.AttrComparisonWithInfo
+import com.dogeby.reliccalculator.core.model.mihomo.index.RelicPiece
+import com.dogeby.reliccalculator.core.model.mihomo.index.RelicSetInfo
+import com.dogeby.reliccalculator.core.model.preset.Preset
+import javax.inject.Inject
+
+class UpdatePresetUseCase @Inject constructor(
+    private val presetRepository: PresetRepository,
+) {
+
+    suspend operator fun invoke(
+        characterId: String,
+        relicSets: List<RelicSetInfo>,
+        pieceMainAffixWeights: Map<RelicPiece, List<AffixWeightWithInfo>>,
+        subAffixWeights: List<AffixWeightWithInfo>,
+        attrComparisons: List<AttrComparisonWithInfo>,
+        isAutoUpdate: Boolean = false,
+    ): Result<Int> {
+        val preset = Preset(
+            characterId = characterId,
+            relicSetIds = relicSets.map { it.id },
+            pieceMainAffixWeights = pieceMainAffixWeights.mapValues { (_, affixWeight) ->
+                affixWeight.map { it.affixWeight }
+            },
+            subAffixWeights = subAffixWeights.map { it.affixWeight },
+            isAutoUpdate = isAutoUpdate,
+            attrComparisons = attrComparisons.map { it.attrComparison },
+        )
+
+        return presetRepository.updatePresets(listOf(preset))
+    }
+}
