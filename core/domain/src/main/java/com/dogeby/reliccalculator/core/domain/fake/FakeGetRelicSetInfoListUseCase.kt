7f35e9aafe1a89be4f35c17998c1972dc00fc278
dogeby <dogeby@gmail.com>
2024-04-04 18:59:37 +0900

    Add FakeGetRelicSetInfoListUseCase (#146)

---
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/fake/FakeGetRelicSetInfoListUseCase.kt
@@ -0,0 +1,16 @@
+package com.dogeby.reliccalculator.core.domain.fake
+
+import com.dogeby.reliccalculator.core.domain.index.GetRelicSetInfoListUseCase
+import com.dogeby.reliccalculator.core.model.mihomo.index.RelicSetInfo
+import com.dogeby.reliccalculator.core.model.mihomo.index.sampleRelicSetInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeGetRelicSetInfoListUseCase : GetRelicSetInfoListUseCase {
+
+    override operator fun invoke(): Flow<List<RelicSetInfo>> {
+        return flow {
+            emit(listOf(sampleRelicSetInfo))
+        }
+    }
+}
--- core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/index/GetRelicSetInfoListUseCase.kt
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/index/GetRelicSetInfoListUseCase.kt
@@ -6,11 +6,16 @@ import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class GetRelicSetInfoListUseCase @Inject constructor(
-    private val gameRepository: GameRepository,
-) {
+interface GetRelicSetInfoListUseCase {
 
-    operator fun invoke(): Flow<List<RelicSetInfo>> {
+    operator fun invoke(): Flow<List<RelicSetInfo>>
+}
+
+class GetRelicSetInfoListUseCaseImpl @Inject constructor(
+    private val gameRepository: GameRepository,
+) : GetRelicSetInfoListUseCase {
+
+    override operator fun invoke(): Flow<List<RelicSetInfo>> {
         return gameRepository.relicSetInfoMap.map {
             it.values.toList()
         }
