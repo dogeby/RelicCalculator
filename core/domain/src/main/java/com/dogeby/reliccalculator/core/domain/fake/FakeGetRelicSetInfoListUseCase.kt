7582097984d60ae9b2fa50b4a3783b1fedd95b44
dogeby <dogeby@gmail.com>
2024-04-06 15:44:15 +0900

    fixup! Add FakeGetRelicSetInfoListUseCase (#146)

--- core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/fake/FakeGetRelicSetInfoListUseCase.kt
+++ core/domain/src/main/java/com/dogeby/reliccalculator/core/domain/fake/FakeGetRelicSetInfoListUseCase.kt
@@ -5,7 +5,9 @@ import com.dogeby.reliccalculator.core.model.mihomo.index.RelicSetInfo
 import com.dogeby.reliccalculator.core.model.mihomo.index.sampleRelicSetInfo
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import org.jetbrains.annotations.TestOnly
 
+@TestOnly
 class FakeGetRelicSetInfoListUseCase : GetRelicSetInfoListUseCase {
 
     override operator fun invoke(): Flow<List<RelicSetInfo>> {
