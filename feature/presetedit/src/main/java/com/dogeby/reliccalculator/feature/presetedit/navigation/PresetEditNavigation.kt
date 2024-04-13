a0b2026548b100654295891f04b3e966cd88f1bd
dogeby <dogeby@gmail.com>
2024-04-13 22:30:47 +0900

    Add PresetEditScreen (#12)

--- feature/presetedit/src/main/java/com/dogeby/reliccalculator/feature/presetedit/navigation/PresetEditNavigation.kt
+++ feature/presetedit/src/main/java/com/dogeby/reliccalculator/feature/presetedit/navigation/PresetEditNavigation.kt
@@ -1,12 +1,18 @@
 package com.dogeby.reliccalculator.feature.presetedit.navigation
 
 import android.net.Uri
+import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.dogeby.reliccalculator.core.common.decoder.StringDecoder
+import com.dogeby.reliccalculator.feature.presetedit.PresetEditRoute
 
-const val PRESET_EDIT_ROUTE = "presetEdit"
+const val PRESET_EDIT_ROUTE = "preset_edit_route"
 internal const val PRESET_ID_ARG = "presetId"
 
 internal class PresetEditArgs(val presetId: String) {
@@ -24,4 +30,21 @@ fun NavController.navigateToPresetEdit(
         route = "$PRESET_EDIT_ROUTE/$encodedId",
         navOptions = navOptions,
     )
+}
+
+fun NavGraphBuilder.presetEditScreen(
+    snackbarHostState: SnackbarHostState,
+    onNavigateUp: () -> Unit,
+) {
+    composable(
+        route = "$PRESET_EDIT_ROUTE/{$PRESET_ID_ARG}",
+        arguments = listOf(
+            navArgument(PRESET_ID_ARG) { type = NavType.StringType },
+        ),
+    ) {
+        PresetEditRoute(
+            snackbarHostState = snackbarHostState,
+            onNavigateUp = onNavigateUp,
+        )
+    }
 }
