a04fc0b20ec4ad3126b41dd8e427a874ff38c470
dogeby <dogeby@gmail.com>
2024-04-15 10:28:52 +0900

    Add fillMaxSize to PresetEditScreen Box modifier
    * Because bottom bar is displayed on top during loading

--- feature/presetedit/src/main/java/com/dogeby/reliccalculator/feature/presetedit/PresetEditScreen.kt
+++ feature/presetedit/src/main/java/com/dogeby/reliccalculator/feature/presetedit/PresetEditScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -162,7 +163,7 @@ fun PresetEditScreen(
             },
         )
     }
-    Box(modifier) {
+    Box(modifier.fillMaxSize()) {
         if (relicSetFiltersUiState is RelicSetFiltersUiState.Success &&
             attrComparisonEditListUiState is AttrComparisonEditListUiState.Success &&
             pieceMainAffixWeightListUiState is PieceMainAffixWeightListUiState.Success &&
